b1f941d472f7609d264e363a2523dcceec0a2ddc
yu-xiaomeng <414017727@qq.com>
2020-07-05 21:32:33 +0800

    should_store_in_1st_m_locker_and_get_m_ticket_when_primary_locker_robot_store_a_m_bag_given_robot_manage_2_m_lockers_both_have_available_capacity

--- src/main/java/com/thoughtworks/robot/PrimaryLockerRobot.java
+++ src/main/java/com/thoughtworks/robot/PrimaryLockerRobot.java
@@ -1,13 +1,22 @@
 package com.thoughtworks.robot;
 
 import com.thoughtworks.AbstractLocker;
+import com.thoughtworks.Bag;
 import com.thoughtworks.Exception.ConfigErrorException;
 import com.thoughtworks.MLocker;
+import com.thoughtworks.Ticket;
 
 import java.util.List;
 
 public class PrimaryLockerRobot {
+    private final List<AbstractLocker> lockers;
+
     public PrimaryLockerRobot(List<AbstractLocker> lockers) {
+        checkLockersConfig(lockers);
+        this.lockers = lockers;
+    }
+
+    private void checkLockersConfig(List<AbstractLocker> lockers) {
         if (lockers.isEmpty()) {
             throw new ConfigErrorException();
         }
@@ -15,7 +24,13 @@ public class PrimaryLockerRobot {
             if(locker instanceof MLocker == false) {
                 throw new ConfigErrorException();
             }
-
         }
+    }
+
+    public Ticket store(Bag mBag) {
+        for(AbstractLocker locker: lockers) {
+            return locker.store(mBag);
+        }
+        return null;
     }
 }
--- src/test/java/com/thoughtworks/PrimaryLockerRobotTest.java
+++ src/test/java/com/thoughtworks/PrimaryLockerRobotTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks;
 
 import com.thoughtworks.Exception.ConfigErrorException;
 import com.thoughtworks.robot.PrimaryLockerRobot;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static java.util.Arrays.asList;
@@ -19,5 +20,18 @@ public class PrimaryLockerRobotTest {
     @Test(expected = ConfigErrorException.class)
     public void should_throw_ConfigErrorException_when_config_primary_locker_robot_given_configuration_list_is_not_contain_any_locker() {
         new PrimaryLockerRobot(asList());
+    }
+
+    @Test
+    public void should_store_in_1st_m_locker_and_get_m_ticket_when_primary_locker_robot_store_a_m_bag_given_robot_manage_2_m_lockers_both_have_available_capacity() {
+        MLocker mLocker1 = new MLocker(5);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(asList(mLocker1, new MLocker(4)));
+        Bag mBag = new Bag(SizeEnum.M);
+
+        Ticket mTicket = primaryLockerRobot.store(mBag);
+
+        Assert.assertNotNull(mTicket);
+        Bag myBag = mLocker1.pickUp(mTicket);
+        Assert.assertEquals(myBag, mBag);
     }
 }
